1096fde97486572096ebc907ac115fc6355dd7c0
Yashwil <yashwil.k@gmail.com>
2025-06-13 17:29:00 +0100

    file metadata

--- chunking.py
+++ chunking.py
@@ -10,11 +10,13 @@ for file in files:
     with open(f"{data_dir}/{file}" ,"r", encoding="utf-8", errors="replace") as f:
         file_text = f.read()
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=128, chunk_overlap=32, # this is the critical line
+        chunk_size=128, chunk_overlap=32, 
     )
     texts = text_splitter.split_text(file_text)
-    for chunked_text in texts:
-        file_texts.append(Document(page_content=chunked_text))
+    for i, chunked_text in enumerate(texts):
+        file_texts.append(Document(page_content=chunked_text,metadata={ 
+                    "doc_title": file.split(".")[0], 
+                    "chunk_num": i}))
 
 from pprint import pprint
 
